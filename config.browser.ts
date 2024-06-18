9c8c7797dcbdf3528e704206187d281bb6205edc
Helena Kohl <helenachristina@gmx.de>
2024-06-18 15:32:42 +0200

    character limit

--- config.browser.ts
+++ config.browser.ts
@@ -1,7 +1,7 @@
 import { samplePhrases } from "./prompts/movie-critic";
-// import { samplePhrases } from "./prompts/tour-guide";
 
 export const appConfig = {
   historyLength: 8,
+  maxMessageLength: 600,
   samplePhrases,
 };
