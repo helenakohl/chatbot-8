79a85af0883db78195f0eed880f2bb9447b116f8
Helena Kohl <helenachristina@gmx.de>
2024-06-18 15:38:23 +0200

    Test

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,8 +1,8 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daniel, your sales agent at BMW</h1>
+    <h1 className="text-2xl font-bold mb-2">👋 Hello, I am Daniel, your sales agent at BMW</h1>
     <p>
-      I have a lot of experience and will try my best to help you find the right car today!
+      I have a lot of experience and will try my best to help you find the right car today! 🚘
     </p>
     <p>
       <a className="underline" href="https://github.com/ascorbic/daneel#readme">
